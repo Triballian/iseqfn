fa6a29879c417b8bae4cb938169efe4c83c7ae77
RustDot <rust.coach@gmail.com>
2020-11-10 23:02:40 -0600

    Logic put together

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,3 @@
-
 //take experssion, gather exponents, if exponent 2 has an expression, not function
 //types of equations
 // Quadratic Equation
@@ -7,49 +6,86 @@
 // Exponential Equation
 // Rational Equation
 //term
-#[derive(Debug)]
+// #[derive(Debug)]
 struct Equation {
     equation: String,
     isfunc: bool,
-    term: Option<Term>,
+    terms: Vec<Term>,
+    //pterms: Vec<Term>,
+    opps: String,
 }
 
 impl Equation {
     fn new(mut eqtion: String) -> Self {
-        eqtion.retain(|c| !c.is_Whitespace());
+        eqtion.retain(|c| !c.is_whitespace());
+        let mut t: String = String::from("");
+        let mut o = String::from("");
+        let mut vt: Option<Vec<Term>> = None;
+        // let mut vt: Option<> = None;
+        for c in eqtion.chars() {
+            // for c in eqtion{
+            //let nexti = i + 1;
+            //if t::as_byte()[nexti] != ('+' | '-' | '*' | '=') {
+            // if c.as_byte() != ('+' | '-' | '*' | '=') {
+            if (c != '+') && (c != '-') && (c != '*') && (c != '=') {
+                t.push(c);
+            } else {
+                o.push(c);
+                if !vt {
+                    vt.unwrap().push(Term::new(t))
+                } else {
+                    vt = vec![Term::new(t)]
+                }
+                t = String::from("");
+            }
+        }
         Self {
             equation: eqtion,
             isfunc: true,
-            term: None,
+            terms: vt,
+            opps: o,
         }
     }
-    fn process(&self) {
-        plintln!("isfunc {}", self.isfunc);
-    }
+    // fn process(&self) {
+    //     for t in self.terms {
+
+    //     }
+
+    //     println!("isfunc {}", self.isfunc);
+    // }
 }
 struct Term {
-    ttype: char, // x or y or n, y , x or nuymber type
-    term: String,
+    exponent: i32,
+    variable: char, // x or y or n, y , x or nuymber type
+    coefficient: String,
 }
 impl Term {
-    fn new (eqtion: String) -> Self {
-        //let i = 1;
-        let t = vec![String];
-        for (i,c) in eqtion.chars().enum(){
-            t.add(c);
-            let nexti = i + 1;
-            if String.as_byte()[nexti] != ("+" | "-" | "*" | "=") {
-                
-            } else {
-                t
+    fn new(t: String) -> Self {
+        let coeff = String::from("");
+        let vr: char;
+        // let cexp: String = String::from("");
+        let exp: i32;
+        let gtvar = false;
+        for c in t.chars() {
+            if gtvar == true {
+                exp = coeff.parse::<i32>().unwrap();
+                continue;
             }
-
+            if (c != 'x') && (c != 'y') && (gtvar == false) {
+                coeff.push(c);
+                continue;
+            }
+            vr = c;
+            gtvar = true;
         }
         Self {
-
+            exponent: exp,
+            variable: vr,
+            coefficient: coeff,
         }
     }
 }
+
 // struct Expression {
 //     base: i32,
 //     var: char,
@@ -63,7 +99,7 @@ impl Term {
 // }
 fn main() {
     let equation = Equation::new(String::from("2x^2 + 3y^2 = 15"));
-    equation.process();
-    println!("Equation: {:?}", equation);
+    // equation.process();
+    // println!("Equation: {:?}", equation);
     println!("is in a function {}", equation.isfunc);
 }
